582862bc107c2b42716f0235ae890b4c9e4030dd
Dan Garro <dobetterwithdan@gmail.com>
2022-09-24 12:07:55 -0400

    started news card group

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,10 +1,12 @@
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap'; 
+import { Card, CardImg, CardTitle, CardText } from 'reactstrap'; 
+
 
 const NewsCard = (props) => {
     return (
         <Card>
-            <CardImg />
+            <CardImg width='90%' src={props.news.image}/>
             <CardTitle>{props.newsheadline.name}</CardTitle>
+            <CardText>{props.news.text}</CardText>
         </Card>
     );
 };
---
+++ src/components/NewsCardGroup.js
@@ -0,0 +1,8 @@
+import CardGroup from 'reactstrap'; 
+import NewsCard from './NewsCard.js'; 
+
+const NewsCardGroup = () => {
+    <CardGroup>
+        <NewsCard 
+    </CardGroup>
+}

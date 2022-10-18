222c3bcac2d4edea050ee8d1241053f92a63c23b
Dan Garro <dobetterwithdan@gmail.com>
2022-10-18 19:57:35 -0400

    working static version with cardgroup

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,10 +1,40 @@
-import React, { useState, useEffect } from 'react'; 
+import React from 'react'; 
 import { Card, CardImg, CardTitle, CardText, CardGroup } from 'reactstrap'; 
-import axios from 'axios'; 
+import NewsImg from '../assets/img/cryptonews.jpg'; 
+import NewsImg2 from '../assets/img/police.jpg'; 
+import NewsImg3 from '../assets/img/bankweathers.jpg'; 
+//import axios from 'axios'; 
 
 //Write api call in JSX inline - Randy did same - View Docs for info 
 // Create a News Card from API Info 
 
+const NewsCard = () => {
+    return (
+        <div>
+            <CardGroup className='mx-3'>
+                <Card>
+                    <CardImg src={NewsImg}></CardImg>
+                    <CardTitle className='h3'>News Headline</CardTitle>
+                    <CardText className='text-muted'>Description of News Article</CardText>
+                </Card>
+                <Card className='px-2'>
+                    <CardImg src={NewsImg2}></CardImg>
+                    <CardTitle className='h3'>News Headline</CardTitle>
+                    <CardText className='text-muted'>Description of News Article</CardText>
+                </Card>
+                <Card>
+                    <CardImg src={NewsImg3}></CardImg>
+                    <CardTitle className='h3'>News Headline</CardTitle>
+                    <CardText className='text-muted'>Description of News Article</CardText>
+                </Card>
+            </CardGroup>
+        </div>
+    )
+};
+
+export default NewsCard; 
+
+/*
 const NewsCard = () => {
     const [data, setData] = useState(null); 
     const [error, setError] = useState(null);
@@ -42,3 +72,4 @@ const NewsCard = () => {
 };
 
 export default NewsCard; 
+*/

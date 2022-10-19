f1e18a475fde7024c88972fb2f7fefcca189cb87
Dan Garro <dobetterwithdan@gmail.com>
2022-10-19 14:55:10 -0400

    adjusted styling via bootstrap of newscard component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import NewsCard from './components/NewsCard'; 
-import NewsCardTest from './components/NewsCardApi';
+//import NewsCardTest from './components/NewsCardApi';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NewsCard />
-      <NewsCardTest />
     </div>
   );
 }
--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -15,17 +15,17 @@ const NewsCard = () => {
                 <Card>
                     <CardImg src={NewsImg}></CardImg>
                     <CardTitle className='h3'>News Headline</CardTitle>
-                    <CardText className='text-muted'>Description of News Article</CardText>
+                    <CardText className='text-muted bg-light'>Description of News Article</CardText>
                 </Card>
                 <Card className='px-2'>
                     <CardImg src={NewsImg2}></CardImg>
                     <CardTitle className='h3'>News Headline</CardTitle>
-                    <CardText className='text-muted'>Description of News Article</CardText>
+                    <CardText className='text-muted bg-light'>Description of News Article</CardText>
                 </Card>
                 <Card>
                     <CardImg src={NewsImg3}></CardImg>
                     <CardTitle className='h3'>News Headline</CardTitle>
-                    <CardText className='text-muted'>Description of News Article</CardText>
+                    <CardText className='text-muted bg-light'>Description of News Article</CardText>
                 </Card>
             </CardGroup>
         </div>
